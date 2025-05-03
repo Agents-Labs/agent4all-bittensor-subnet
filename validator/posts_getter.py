ef0c1b024f96328c4a050eb0ca817fa56e0a04d6
jordanpfost0202 <jordanpfost202323@gmail.com>
2025-05-03 11:55:14 -0500

    feat/update the project structure for better emmisssion

---
+++ validator/posts_getter.py
@@ -0,0 +1,50 @@
+from typing import List, Any, Optional
+from datetime import datetime, UTC
+from fiber.logging_utils import get_logger
+import httpx
+import os
+from interfaces.types import Tweet
+
+logger = get_logger(__name__)
+
+
+class PostsGetter:
+    def __init__(self, netuid: int):
+        self.netuid = netuid
+        self.api_key = os.getenv("API_KEY", None)
+        self.api_url = os.getenv("API_URL", "https://test.protocol-api.masa.ai")
+        self.httpx_client = httpx.AsyncClient(
+            base_url=self.api_url,
+            headers={"Authorization": f"Bearer {self.api_key}"},
+            timeout=120,
+        )
+
+    async def get(self) -> List[Optional[Tweet]]:
+        a_week_ago_in_seconds = 7 * 24 * 60 * 60
+        since = int(datetime.now(UTC).timestamp()) - a_week_ago_in_seconds
+        posts = await self.fetch_posts_from_api(since)
+        return posts
+
+    async def fetch_posts_from_api(self, since) -> List[Optional[Tweet]]:
+        """Fetch posts from the API and update self.posts"""
+        posts = []
+        try:
+            response = await self.httpx_client.get(
+                f"{self.api_url}/v1.0.0/subnet59/miners/posts?since={since}"
+            )
+            if response.status_code == 200:
+                posts_data = dict(response.json())
+                posts = posts_data.get("posts", [])
+                logger.info(f"Successfully fetched {len(posts)} posts from API")
+            else:
+                logger.error(
+                    f"Failed to fetch posts, status code: {response.status_code}, message: {response.text}"
+                )
+        except httpx.RequestError as e:
+            logger.error(f"Request error occurred: {str(e)}")
+        except httpx.HTTPStatusError as e:
+            logger.error(f"HTTP error occurred: {str(e)}")
+        except Exception as e:
+            logger.error(f"Unexpected exception occurred: {str(e)}")
+        finally:
+            return posts
--- worker_api/README.md
+++
@@ -1,9 +0,0 @@
-# For internal use only
-
-
-# To build valdiation api image
-From root directory
-```
-docker build -t vapi:v12 -f worker_api/vapi.Dockerfile .
-```
-
